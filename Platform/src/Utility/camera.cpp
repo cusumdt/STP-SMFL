0f75315d4c113052758df552190f09df81bd6519
unknown <pehuen-testa@hotmail.com>
2018-11-16 12:55:12 -0300

    Camera class2

---
+++ Platform/src/Utility/camera.cpp
@@ -0,0 +1,28 @@
+#include "camera.h"
+
+Camera::Camera()
+{
+}
+
+Camera::~Camera()
+{
+}
+
+void Camera::setPos(float posX)
+{
+}
+void Camera::setPos(float posY)
+{
+}
+void Camera::setPos(sf::Vector2f pos)
+{
+}
+void Camera::setRotation(float rotation)
+{
+}
+void Camera::movementCamera()
+{
+}
+void Camera::shakeCamera()
+{
+}
--- Platform/src/Utility/camera.h
+++ Platform/src/Utility/camera.h
@@ -1,25 +1,25 @@
 #ifndef CAMERA_H
 #define CAMERA_H
-
+#include "../Logic/game.h"
 class Camera
 {
 private:
-	float _posX;
-	float _posY;
+	sf::Vector2f _pos
 	float _rotation;
 public:
 	Camera();
 	~Camera();
-	
+	float getPosX() { return _pos.x; }
+	float getPosY() { return _pos.y; }
+	float getPos() {return _pos}
+	float getRotation() { return _rotation; }
+	void setPos(float posX);
+	void setPos(float posY);
+	void setPos(sf::Vector2f pos);
+	void setRotation(float rotation);
+	void movementCamera();
+	void shakeCamera();
 };
-
-Camera::Camera()
-{
-}
-
-Camera::~Camera()
-{
-}
 
 #endif // !CAMERA_H
 
