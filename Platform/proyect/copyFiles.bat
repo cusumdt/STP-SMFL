72586a9bd5bab45b2b72caef72406f723e9edd24
insanefury <justdarsan@gmail.com>
2018-12-06 02:52:11 -0300

    Fixed .bat copyfiles error

--- Platform/proyect/copyFiles.bat
+++ Platform/proyect/copyFiles.bat
@@ -2,8 +2,8 @@
 
 echo Copiando Archivos...
 
-set SolutionDir=%1
-set TargetDir=%2
+set SolutionDir=%~1
+set TargetDir=%~2
 
 echo SolutionDir
 echo TargetDir
@@ -11,6 +11,8 @@ echo TargetDir
 XCOPY "%SolutionDir%libs\SFML-2.5.1\bin" "%TargetDir%" /e /h /y /s
 
 XCOPY "%SolutionDir%libs\STP\bin\*.dll" "%TargetDir%" /e /h /y /s
+
+XCOPY "%SolutionDir%libs\TGUI-0.8\bin\*.dll" "%TargetDir%" /e /h /y /s
 
 MKDIR "%TargetDir%res"
 XCOPY "%SolutionDir%res" "%TargetDir%res" /e /h /y /s
