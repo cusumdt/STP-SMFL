9cb980efcf6786694aa19a44b5192cebb67aab7e
unknown <pehuen-testa@hotmail.com>
2018-12-07 12:01:26 -0300

    Fixed .bat

--- Platform/proyect/copyFiles.bat
+++ Platform/proyect/copyFiles.bat
@@ -8,11 +8,11 @@ set TargetDir=%~2
 echo SolutionDir
 echo TargetDir
 
-XCOPY "%SolutionDir%libs\SFML-2.5.1\bin" "%TargetDir%" /e /h /y /s
+XCOPY "%SolutionDir%libs\SFML-2.5.1\bin\release\*.dll" "%TargetDir%" /e /h /y /s
 
-XCOPY "%SolutionDir%libs\STP\bin\*.dll" "%TargetDir%" /e /h /y /s
+XCOPY "%SolutionDir%libs\STP\bin\release\*.dll" "%TargetDir%" /e /h /y /s
 
-XCOPY "%SolutionDir%libs\TGUI-0.8\bin\*.dll" "%TargetDir%" /e /h /y /s
+XCOPY "%SolutionDir%libs\TGUI-0.8\bin\release\*.dll" "%TargetDir%" /e /h /y /s
 
 MKDIR "%TargetDir%res"
 XCOPY "%SolutionDir%res" "%TargetDir%res" /e /h /y /s
---
+++ Platform/proyect/copyFiles_d.bat
@@ -0,0 +1,18 @@
+@echo off
+
+echo Copiando Archivos...
+
+set SolutionDir=%~1
+set TargetDir=%~2
+
+echo SolutionDir
+echo TargetDir
+
+XCOPY "%SolutionDir%libs\SFML-2.5.1\bin\debug\*.dll" "%TargetDir%" /e /h /y /s
+
+XCOPY "%SolutionDir%libs\STP\bin\debug\*.dll" "%TargetDir%" /e /h /y /s
+
+XCOPY "%SolutionDir%libs\TGUI-0.8\bin\debug\*.dll" "%TargetDir%" /e /h /y /s
+
+MKDIR "%TargetDir%res"
+XCOPY "%SolutionDir%res" "%TargetDir%res" /e /h /y /s
